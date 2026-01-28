c4038eb75ab4d587a6b666bc6eddd8e253e6cacf
lenadegtereva25-ui <lenadegtereva25@gmail.com>
2026-01-28 22:19:08 +0300

    Add files via upload

---
+++ lab 1.2.py
@@ -0,0 +1,13 @@
+BYTES_ONE_CHAR = 4  # размер одного символа в байтах
+
+pages = 100
+lines = 50
+chars = 25
+
+total_chars = pages * lines * chars
+total_bytes = total_chars * BYTES_ONE_CHAR
+
+disk_size = 1.44 * 1024 * 1024  # размер в байтах
+num_books = int(disk_size // total_bytes)
+
+print("Количество книг, помещающихся на дискету:", num_books)
